d58924a2bdcb85b8518f1c979e2691d747e57bed
Sorae1118 <ekawls6289@gmail.com>
2022-12-07 10:46:39 +0900

    dnd 기능 수정 조담진

--- 예제 파일/dnd_example.js
+++ 예제 파일/dnd_example.js
@@ -1,15 +1,9 @@
-function console(e) {
-    if(document.querySelectorAll('p').length === 10) {
-      document.querySelectorAll('p').forEach(e => e.remove())
-    }
-    const p = document.createElement('p');
-    p.textContent = e;
-    document.body.append(p);
+
+window.onload = function() {
+  const item = document.querySelector(".item");
+
+  item.addEventListener("dragstart", (e) => {
+    console("드래그를 시작하면 발생하는 이벤트");
+    
+  });  
 }
-
-const item = document.querySelector(".item");
-
-item.addEventListener("dragstart", (e) => {
-  console("드래그를 시작하면 발생하는 이벤트");
-  
-});
