b8ee3179fd5a9380802e6d92082ff05bfb8379bf
Sorae1118 <ekawls6289@gmail.com>
2022-12-17 19:54:38 +0900

    컨테이너 변수 해결 - 담진

--- 핵심 기능/hideTest.js
+++ 핵심 기능/hideTest.js
@@ -1,6 +1,54 @@
-$(window).ready(function(){
-	$("#dmd").hide();
+$(window).ready(function () {
+    $("#dmd").hide();
     $(".d1").hide();
     $("#dmd").fadeIn(1000);
     $(".d1").delay(1000).fadeIn(1000);
 });
+
+window.onload = function () {
+    const draggables = document.querySelectorAll(".draggable");
+    const containers = document.querySelectorAll(".container2");
+
+    draggables.forEach(draggable => {
+        draggable.addEventListener("dragstart", () => {
+            draggable.classList.add("dragging");
+        });
+
+        draggable.addEventListener("dragend", () => {
+            draggable.classList.remove("dragging");
+        });
+    });
+    containers.forEach(container => {
+        container.addEventListener("dragover", e => {
+            e.preventDefault();
+            const afterElement = getDragAfterElement(container, e.clientX);
+            const draggable = document.querySelector(".dragging");
+            if (afterElement === undefined) {
+                container.appendChild(draggable);
+            } else {
+                container.insertBefore(draggable, afterElement);
+            }
+        });
+    });
+
+    function getDragAfterElement(container, x) {
+        const draggableElements = [
+            ...container.querySelectorAll(".draggable:not(.dragging)"),
+        ];
+
+        return draggableElements.reduce(
+            (closest, child) => {
+                const box = child.getBoundingClientRect();
+                const offset = x - box.left - box.width / 2;
+                // console.log(offset);
+                if (offset < 0 && offset > closest.offset) {
+                    return { offset: offset, element: child };
+                } else {
+                    return closest;
+                }
+            },
+            { offset: Number.NEGATIVE_INFINITY },
+        ).element;
+    }
+}
+
