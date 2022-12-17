122030142737dc98b6670ee6aba07129552ffb57
gildong <gildong@naver.com>
2022-12-17 18:48:41 +0900

    하이

--- 정호/js/main.js
+++ 정호/js/main.js
@@ -1,40 +1,45 @@
-const fill = document.querySelector('.fill'); //fill 박스 셀렉트
-const empties = document.querySelectorAll('.empty'); // empty 클래스는 다수이기 때문에 selectorAll 사용
+const fill = document.querySelector('.fill'); 
+    const empties = document.querySelectorAll('.empty');
  
-   // Fill listeners
-fill.addEventListener('dragstart', dragStart); //드래그시작    
-fill.addEventListener('dragend', dragEnd);
+    // Fill listeners
+    fill.addEventListener('dragstart', dragStart);      
+    fill.addEventListener('dragend', dragEnd); 
+ 
 
-//드래그 이벤트
-for(const empty of empties) {
-    empty.addEventListener('dragover', dragOver);
-    empty.addEventListener('dragenter', dragEnter);
-    empty.addEventListener('dragleave', dragLeave);
-    empty.addEventListener('drop', dragDrop);
+for (const empty of empties) {
+  empty.addEventListener('dragover', dragOver); 
+  empty.addEventListener('dragenter', dragEnter); 
+  empty.addEventListener('dragleave', dragLeave); 
+  empty.addEventListener('drop', dragDrop); 
 }
-
-//드래그이벤트 작동할 함수
+ 
+// Drag Functions
+ 
+//dragstart  '회색 테두리가 생김' 동작
 function dragStart() {
-    this.className += ' hold';
-    setTimeout(() => (this.className = 'invisible'), 0);
+  this.className += ' hold';
+  setTimeout(() => (this.className = 'invisible'), 0);
 }
-
-
+//드래그 하다가 마우스 버튼을 놓는 순간 fill 박스가 발생
 function dragEnd() {
-    this.className = 'fill';
+  this.className = 'fill';
 }
-
+ 
 function dragOver(e) {
-    empties.preventDefault();
+  e.preventDefault();
 }
+ 
+//drag enter 시 테두리가 dashed로 변함
 function dragEnter(e) {
-    empties.preventDefault();
-    this.className += 'hovered';
+  e.preventDefault();
+  this.className += ' hovered';
 }
+// 드래그요소가 객체를 위를 벗어나면 empty 박스가 됨.
 function dragLeave() {
-    this.className = 'empty';
+  this.className = 'empty';
 }
+// dragdrop : 드래그를 드랍할때 빈 박스안에 fill 이 추가됨.==채워짐
 function dragDrop() {
-    this.className = 'empty'; 
-    this.append(fill);
+  this.className = 'empty';
+  this.append(fill);
 }
