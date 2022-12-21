1bc325a6e18c90fd46d6d02107607629c23de30c
gildong <gildong@naver.com>
2022-12-21 19:09:36 +0900

    drag

--- 웹 프로젝트/engWord.js
+++ 웹 프로젝트/engWord.js
@@ -72,50 +72,22 @@ function selectQuiz() {
     getCardsFromWords2(words);
 }
 
-//=========================== 이벤트 핸들러 연결 =======================================
-function addEvent() { 
-    let cardArray = document.querySelectorAll(".card");
-    for(let card of cardArray) {
-        card.addEventListener("dragstart", onDragStartCard);
-        card.addEventListener("dragend", onDragEndCard);
-        card.addEventListener("dragover", onDragOverCard);
-        card.addEventListener("dragleave", onDragLeaveCard);
-        card.addEventListener("drop", onDropCard);
-    }
-    
-    let boxArray = document.querySelectorAll(".box");
-    for(let box of boxArray) {
-        box.addEventListener("dragover",onDragOverBox);
-        box.addEventListener("dragleave",onDragLeaveBox);
-        box.addEventListener("drop", onDropBox);
-    }
-}
-window.onload = function () {
-    let items = document.getElementsByClassName("item");
-    console.log(items);
-    for(let item of items) {
-        item.addEventListener("click", selectQuiz);
-    }
-    addEvent();
-}
-
-//=============================수업시간 드래그 앤 드롭================================
-//===============================================================================
+//=========================== 이벤트 핸들러 연결 =======================
 function onDragStartCard(ev) {
-    draggingCard = this;   
-    this.classList.add("draggingCard");
+    draggingCard = this;    //이동 중인 특정 카드 this
+    this.classList.add("draggingCard"); // 클래스 추가. 모든 HTML 속성은 classList 를 가지고 있음. 즉 여러개의 클래스를 쓸 수 있음
 }
 
 function onDragEndCard(ev) {
-    ev.preventDefault(); 
-    this.classList.remove("draggingCard");
+    ev.preventDefault(); //웹 브라우저의 디폴트 처리를 하지 못하도록 한다. ???????
+    this.classList.remove("draggingCard"); //클래스를 제거해줌
     draggingCard = null;
-
+    //dragOverBox가 null이 아니면 처리할 내용 => 즉 dragleave가 발생하지 않았을 때
     if(dragOverBox) {
-        dragOverBox.classList.remove("overBox");
+        dragOverBox.classList.remove("overBox"); //this는 드래그 이벤트임 this를 쓰면 안됨
         dragOverBox = null;
     }
-    
+    //dragOverCar가 null이 아니면
     if(dragOverCard) {
         dragOverCard.classList.remove("overCard"); 
         dragOverCard = null;
@@ -124,7 +96,7 @@ function onDragEndCard(ev) {
 
 function onDragOverCard(ev) {
     ev.preventDefault();
-    ev.stopPropagation(); 
+    ev.stopPropagation(); //dragOver 이벤트가 부모 노드로 전파되는 것을 차단한다. 즉 박스에 영향을 주지 않기 위함.
     dragOverCard = this;
     this.classList.add("overCard");
 }
@@ -136,13 +108,15 @@ function onDragLeaveCard(ev) {
 }
 
 function onDropCard(ev) {
-    this.parentNode.insertBefore(draggingCard, this); 
+    this.parentNode.insertBefore(draggingCard, this); //모든 객체는 parentNode가 있음. insertBefore(삽입할 객체, 뒤에 위치하는 객체)
 }
-//========박스===================================================================
+//==============================================
+// class box 객체 이벤트 핸들러 영역
+//==============================================
 function onDragOverBox(ev) {
     ev.preventDefault();
     dragOverBox = this;
-    this.classList.add("overBox");
+    this.classList.add("overBox");  //this 의 정확한 의미를 알아야 함
 }
 
 function onDragLeaveBox(ev) {
@@ -152,15 +126,32 @@ function onDragLeaveBox(ev) {
 }
 
 function onDropBox(ev) {
-    dragOverBox.appendChild(draggingCard); 
+    dragOverBox.appendChild(draggingCard); //append는 맨 뒤에 추가함
 }
-
-window.onload = function () {
+//==============================================
+window.onload = function() {
     let items = document.getElementsByClassName("item");
     console.log(items);
     for(let item of items) {
         item.addEventListener("click", selectQuiz);
     }
-    dragCards();
+    // 단어 카드들을 먼저 생성한다
+    //getCardsFromWords(quizWords);
+    selectQuiz(1);
+    // card 객체 이벤트 핸들러 연결하기
+    let cardArray = document.querySelectorAll(".card"); // 모든 카드??
+    for(let card of cardArray) {
+        card.addEventListener("dragstart", onDragStartCard);
+        card.addEventListener("dragend", onDragEndCard);
+        card.addEventListener("dragover", onDragOverCard);
+        card.addEventListener("dragleave", onDragLeaveCard);
+        card.addEventListener("drop", onDropCard);
+    }
+    // box 객체 이벤트 핸들러 연결하기
+    let boxArray = document.querySelectorAll(".box");
+    for(let box of boxArray) {
+        box.addEventListener("dragover",onDragOverBox);
+        box.addEventListener("dragleave",onDragLeaveBox);
+        box.addEventListener("drop", onDropBox);
+    }
 }
-//==============================================
