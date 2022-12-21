32be476b35fface42f93e83c9b223fea392a6e63
Sorae1118 <ekawls6289@gmail.com>
2022-12-21 18:03:04 +0900

    새로운 폴더

---
+++ 웹 프로젝트/quiz.js
@@ -0,0 +1,21 @@
+let quizSet = {
+    topic: "기말과제 예제 제시",
+    author: "서정호",
+    quiz : [
+        {
+        eng: "so programming is the way I think we should be dbing that.",
+        kor: "그래서 프로그램이 우리가 그렇게 해야만 하는 바로 그 방법이라고 생각해.",
+        }
+        ,
+        {
+        eng: "I need you to help me",
+        kor: "네가 도와주면 좋겠어",
+        }
+        ,
+        {
+        eng: "i like listening to music",
+        kor: "나는음악듣는걸좋아해", 
+        }
+    ]
+
+}
