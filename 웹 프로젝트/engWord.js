e99b3f8165f17e3535a85302fca18724ad354eae
Sorae1118 <ekawls6289@gmail.com>
2022-12-21 23:33:28 +0900

    asdf

--- 웹 프로젝트/engWord.js
+++ 웹 프로젝트/engWord.js
@@ -30,20 +30,24 @@ $(window).ready(function () {
         window.scrollTo({ top: location, behavior: "smooth" });
     })
 
-    $("#checkBtn").click(function(){
+    $("#checkBtn").click(function() {
+        let tf = check();
         $(".container3").show();
         let location = document.querySelector(".container3").offsetTop;
         window.scrollTo({ top: location, behavior: "smooth" });
-        $("#loading").delay(3000).fadeOut(1000);
-        if(check()){
-
-        }else{
-         $(".container4").delay(3000).fadeIn(2000);
-
+        $("#loading").fadeOut(2000);
+        if(tf) {
+            $(".container4").delay(1000).fadeIn(2000);
+            let location = document.querySelector(".container4").offsetTop;
+            window.scrollTo({ top: location, behavior: "smooth" });
+        }else {
+            $(".container5").delay("slow").fadeIn(2000);
+            let location = document.querySelector(".container5").offsetTop;
+            window.scrollTo({ top: location, behavior: "smooth" });
         }
     });
 
-    $("#btn2").click( function(){
+    $("#btn2").click( function() {
         console.log($(".item"));
         $(".item").not().removeClass("selectitem");
         $(".item").fadeIn(1500);
